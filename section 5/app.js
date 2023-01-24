a15e80487dfaf3cc7abcebdeffab09db1177e334
alistkov <antonlistkov@gmail.com>
2023-01-24 20:59:25 +0300

    video 113

---
+++ section 5/app.js
@@ -0,0 +1,15 @@
+let name = 'John';
+
+if (name === 'John') {
+  var hobbies = ['sports', 'cooking'];
+  console.log(hobbies);
+}
+
+function greet() {
+  let age = 30;
+  let name = 'Bob';
+  console.log(name, age);
+}
+
+console.log(name, hobbies);
+greet();
