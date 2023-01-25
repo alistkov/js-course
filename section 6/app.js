39c4fdf65e25e60632e85e5f6defbc2e5209cc7c
alistkov <antonlistkov@gmail.com>
2023-01-25 23:27:24 +0300

    video 131

--- section 6/app.js
+++ section 6/app.js
@@ -1,19 +1,31 @@
 const startGameBtn = document.getElementById('start-game-btn');
 
-// const start = function() {
-//   console.log('Game is starting...');
-// }
+const ROCK = 'ROCK';
+const PAPER = 'PAPER';
+const SCISSORS = 'SCISSORS';
+const DEFAULT_USER_CHOISE = ROCK;
 
-// const person = {
-//   greet: function greet() {
-//     console.log('Hello there!');
-//   }
-// };
+let gameIsRunning = false;
 
-// person.greet();
+const getPlayerChoice = function () {
+  const selection = prompt(
+    `${ROCK}, ${PAPER} or ${SCISSORS}?`,
+    ''
+  ).toUpperCase();
+  if (selection !== ROCK && selection !== PAPER && selection !== SCISSORS) {
+    alert(`Invalid choise! We chose ${DEFAULT_USER_CHOISE} for you!`);
+    return DEFAULT_USER_CHOISE;
+  }
 
-// console.log(typeof startGame);
+  return selection;
+};
 
-startGameBtn.addEventListener('click', function startGame() {
-  console.log('Game is starting...', age);
+startGameBtn.addEventListener('click', function () {
+  if (gameIsRunning) {
+    return;
+  }
+  gameIsRunning = true;
+  console.log('Game is starting...');
+  const playerSelection = getPlayerChoice();
+  console.log(playerSelection);
 });
