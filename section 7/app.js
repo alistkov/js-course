7c081f675c8a7d279bf180dc2de4414f54325b82
alistkov <antonlistkov@gmail.com>
2023-02-08 22:55:57 +0300

    video 159

--- section 7/app.js
+++ section 7/app.js
@@ -1,16 +1,4 @@
-const h1 = document.getElementById('main-title');
-h1.textContent = 'Some new title!';
-h1.style.color = 'white';
-h1.style.backgroundColor = 'black';
+const ul = document.body.firstElementChild.nextElementSibling;
+const firstLi = ul.firstElementChild;
 
-const li = document.querySelector('li:last-of-type');
-li.textContent = `${li.textContent} (Changed!)`;
-
-const body = document.body;
-body.style.backgroundColor = '#f1f1f1';
-
-const listElements = document.querySelectorAll('li');
-
-for (const item of listElements) {
-  console.dir(item);
-}
+console.log(firstLi);
