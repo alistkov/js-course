9fb45e60e47ea165f1e16826b8da5eabf8014202
alistkov <antonlistkov@gmail.com>
2023-02-09 11:47:07 +0300

    video 176

--- section 7/assets/scripts/app.js
+++ section 7/assets/scripts/app.js
@@ -4,8 +4,34 @@ const showAddMovieModalButton = document.querySelector('header button');
 const hideAddMovieModal = addMovieModal.querySelector('.btn--passive');
 const confirmAddMovieButton = addMovieModal.querySelector('.btn--success');
 const inputs = addMovieModal.querySelectorAll('input');
+const entryTextSection = document.getElementById('entry-text');
 
 const movies = [];
+
+const updateUI = () => {
+  if (movies.length === 0) {
+    entryTextSection.style.display = 'block';
+  } else {
+    entryTextSection.style.display = 'none';
+  }
+};
+
+const renderNewMovieElement = (title, imageUrl, rating) => {
+  const movieEl = document.createElement('li');
+  movieEl.classList.add('movie-element');
+  movieEl.innerHTML = `
+    <div class="movie-element__image">
+      <img src="${imageUrl}" alt="${title}" />
+    </div>
+    <div class="movie-element__info">
+        <h2>${title}</h2>
+        <p>${rating}/5 stars</p>
+    </div>
+  `;
+
+  const listRoot = document.getElementById('movie-list');
+  listRoot.append(movieEl);
+};
 
 const clearMovieInputs = () => {
   for (const input of inputs) {
@@ -37,11 +63,13 @@ const addMovieHandler = () => {
   const newMovie = {
     title: titleValue,
     image: imageUrlValue,
-    ratingValue: ratingValue,
+    rating: ratingValue,
   };
   movies.push(newMovie);
   hideModalAndBackdrop();
   clearMovieInputs();
+  renderNewMovieElement(newMovie.title, newMovie.image, newMovie.rating);
+  updateUI();
 };
 
 
