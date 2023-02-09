dfe68f7579bf110a35216d28c7c87d90827c3ade
alistkov <antonlistkov@gmail.com>
2023-02-09 10:58:29 +0300

    video 173

--- section 7/assets/scripts/app.js
+++ section 7/assets/scripts/app.js
@@ -1,3 +1,12 @@
 const addMovieModal = document.getElementById('add-modal');
 const backdrop = document.getElementById('backdrop');
-const showAddModalButton = document.querySelector('header button');
+const showAddMovieModalButton = document.querySelector('header button');
+
+const showAddMovieModalAndBackdrop = () => {
+  addMovieModal.classList.add('visible');
+  backdrop.classList.add('visible');
+};
+
+showAddMovieModalButton.addEventListener('click', () => {
+  showAddMovieModalAndBackdrop();
+});
