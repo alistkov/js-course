9c577cc8a7e0af5ac9145d35cf9dd1381e13a04f
alistkov <antonlistkov@gmail.com>
2023-02-09 11:24:54 +0300

    video 174

--- section 7/assets/scripts/app.js
+++ section 7/assets/scripts/app.js
@@ -2,17 +2,31 @@ const addMovieModal = document.getElementById('add-modal');
 const backdrop = document.getElementById('backdrop');
 const showAddMovieModalButton = document.querySelector('header button');
 const hideAddMovieModal = addMovieModal.querySelector('.btn--passive');
+const confirmAddMovieButton = addMovieModal.querySelector('.btn--success');
+const inputs = addMovieModal.querySelectorAll('input');
 
-const toggleBackdrop = () => {
-  backdrop.classList.toggle('visible');
+const showModalAndBackdrop = () => {
+  addMovieModal.classList.add('visible');
+  backdrop.classList.add('visible');
 };
 
-const toggleMovieModal = () => {
-  addMovieModal.classList.toggle('visible');
-  toggleBackdrop();
-}
+const hideModalAndBackdrop = () => {
+  addMovieModal.classList.remove('visible');
+  backdrop.classList.remove('visible');
+};
+
+const addMovieHandler = () => {
+  const titleValue = inputs[0].value;
+  const imageUrlValue = inputs[1].value;
+  const ratingValue = inputs[2].value;
+
+  if (titleValue.trim() === '' || imageUrlValue.trim() === '' || ratingValue.trim() === '' || +ratingValue < 1 || +ratingValue > 5) {
+    alert('Please enter valid values (rating between 1 and 5).');
+  }
+};
 
 
-showAddMovieModalButton.addEventListener('click', toggleMovieModal);
-backdrop.addEventListener('click', toggleMovieModal);
-hideAddMovieModal.addEventListener('click', toggleMovieModal);
+showAddMovieModalButton.addEventListener('click', showModalAndBackdrop);
+backdrop.addEventListener('click', hideModalAndBackdrop);
+hideAddMovieModal.addEventListener('click', hideModalAndBackdrop);
+confirmAddMovieButton.addEventListener('click', addMovieHandler)
