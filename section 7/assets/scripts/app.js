2965169582ef82feee8354f2fab656c195cb78c6
alistkov <antonlistkov@gmail.com>
2023-02-09 10:51:34 +0300

    video 172

---
+++ section 7/assets/scripts/app.js
@@ -0,0 +1,3 @@
+const addMovieModal = document.getElementById('add-modal');
+const backdrop = document.getElementById('backdrop');
+const showAddModalButton = document.querySelector('header button');
